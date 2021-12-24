dbaaabedb60c7ce7f3945d80614f3cbd2e47eed1
WolfCodeX <96460179+WolfCodeX@users.noreply.github.com>
2021-12-24 16:34:59 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
-const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'CodeXTeam' // ubah jadi nama lu
+const listkey = ["CodexApi","WolfCodeX","FoxyCodeX"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
